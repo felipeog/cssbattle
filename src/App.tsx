905e7b138ad517073427265150520669e616c60c
felipeog <felipeog476@gmail.com>
2022-02-23 01:21:36 -0300

    Render challenges no App

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
-import './App.css'
+import { Card } from './components/Card'
+import * as rawChallenges from './challenges'
 
 export function App() {
-  return <div className="App">App</div>
+  const challenges = Object.values(rawChallenges)
+
+  return (
+    <div className="App">
+      {challenges.map((challenge) => (
+        <Card key={challenge.title} {...challenge} />
+      ))}
+    </div>
+  )
 }
