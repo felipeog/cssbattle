cbf449b86ea1917c935f2dc44cc5ab87475b9700
felipeog <felipeog476@gmail.com>
2022-02-23 01:20:30 -0300

    Add challenges

---
+++ src/challenges/004.tsx
@@ -0,0 +1,6 @@
+export default {
+  title: '#4 - Ups n Downs',
+  url: 'https://cssbattle.dev/play/4',
+  solution:
+    '<a a><a b><a c><style>body{background:#62306D}a{position:fixed;background:#F7EC7D;width:100;height:100;border-radius:0 0 50% 50%}a[a]{top:50%;left:50}a[b]{top:50;left:150;transform:rotate(180deg)}a[c]{top:50%;right:50}</style>',
+}
---
+++ src/challenges/index.ts
@@ -0,0 +1,4 @@
+export { default as challenge1 } from './001'
+export { default as challenge2 } from './002'
+export { default as challenge3 } from './003'
+export { default as challenge4 } from './004'
