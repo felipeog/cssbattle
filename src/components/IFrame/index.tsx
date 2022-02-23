9bf41ffc6704fc110ff439b29947e1127726b9b3
felipeog <felipeog476@gmail.com>
2022-02-23 01:20:47 -0300

    Add IFrame component

---
+++ src/components/IFrame/index.tsx
@@ -0,0 +1,27 @@
+import { IframeHTMLAttributes, useState } from 'react'
+import { createPortal } from 'react-dom'
+
+import styles from './index.module.css'
+
+export type IFrameProps = IframeHTMLAttributes<HTMLIFrameElement>
+
+// https://dev.to/graftini/rendering-in-an-iframe-in-a-react-app-2boa
+export function IFrame({ children, ...props }: IFrameProps) {
+  const [iframeRef, setIframeRef] = useState<HTMLIFrameElement>()
+
+  const head = {
+    children: <style>{`body{overflow:hidden}`}</style>,
+    container: iframeRef?.contentDocument?.head,
+  }
+  const body = {
+    children: children,
+    container: iframeRef?.contentDocument?.body,
+  }
+
+  return (
+    <iframe className={styles.container} ref={setIframeRef} {...props}>
+      {head.container && createPortal(head.children, head.container)}
+      {body.container && createPortal(body.children, body.container)}
+    </iframe>
+  )
+}
