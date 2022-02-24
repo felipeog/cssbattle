f97d02d9b14f6f57b27c167efaf92ecc721487dd
felipeog <felipeog476@gmail.com>
2022-02-24 19:11:18 -0300

    Add targets 7 and 8

---
+++ src/targets/008.tsx
@@ -0,0 +1,10 @@
+export default {
+  title: 'Target #8 - Forking Crazy',
+  url: 'https://cssbattle.dev/play/8',
+  solution:
+    '<a a><a b><a c><style>body{background:#6592CF}a{position:fixed;background:#060F55}a[a],a[b]{left:50%;transform:translate(-50%)}a[a]{bottom:0;width:20;height:60}a[b]{bottom:50;width:140;height:140;border-radius:0 0 50% 50%}a[c]{top:50;left:130;width:20;height:110;border-radius:10px;box-shadow:20px 0 #6592CF,40px 0 #060F55,60px 0 #6592CF,80px 0 #060F55,100px 0 #6592CF,120px 0 #060F55}</style>',
+  battle: {
+    title: 'Battle #1 - Pilot Battle',
+    url: 'https://cssbattle.dev/battle/1',
+  },
+}
