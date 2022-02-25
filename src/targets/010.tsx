8d9b0cf7dcfdfb47cbb1c08b174878269fedf94b
felipeog <felipeog476@gmail.com>
2022-02-25 00:18:09 -0300

    Add targets 10, 11 and 12

---
+++ src/targets/010.tsx
@@ -0,0 +1,10 @@
+export default {
+  title: 'Target #10 - Cloaked Spirits',
+  url: 'https://cssbattle.dev/play/10',
+  solution:
+    '<a a><a b><a c><style>body{background:#62306D}a{position:fixed;top:200;left:50;width:100;height:200;background:#F7EC7D;}a::after{content:"";position:absolute;top:-50;width:60;height:60;background:#E38F66;border-radius:50%;border:20px solid #AA445F}a[b]{top:100;left:150}a[b]::after{background:#AA445F;border-color:#E38F66}a[c]{left:250}</style>',
+  battle: {
+    title: 'Battle #1 - Pilot Battle',
+    url: 'https://cssbattle.dev/battle/1',
+  },
+}
