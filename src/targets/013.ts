f1852f7192a4b5256b8036c23521646db3a705a8
felipeog <felipeog476@gmail.com>
2022-02-25 17:46:00 -0300

    Update start script; Add target 13

---
+++ src/targets/013.ts
@@ -0,0 +1,10 @@
+import { BATTLES_MAP } from '../consts/battlesMap'
+
+export default {
+  targetId: 'target013',
+  title: 'Target #13 - Totally Triangle',
+  url: 'https://cssbattle.dev/play/13',
+  solution:
+    '<a><style>body{background:#0B2429}a{position:fixed;top:0;left:0;border:70px solid #F3AC3C;border-bottom-color:transparent;border-right-color:transparent}</style>',
+  battle: BATTLES_MAP['battle002'],
+}
