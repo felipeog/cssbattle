5863a573e1d1c3a4e8b521184e26c32ff858b22c
felipeog <felipeog476@gmail.com>
2022-02-27 21:13:39 -0300

    Add 19 and 20

---
+++ src/targets/020.ts
@@ -0,0 +1,10 @@
+import { BATTLES_MAP } from '../consts/battlesMap'
+
+export default {
+  targetId: 'target020',
+  title: 'Target #20 - Ticket',
+  url: 'https://cssbattle.dev/play/20',
+  solution:
+    '<a a><a b><a c><style>body{background:#62306d}a{position:fixed}a[a]{top:100;left:100;width:200;height:100;background:linear-gradient(90deg,#f7ec7d 70%,#e38f66 70%)}a[b],a[c]{border-radius:50%;background:#62306d}a[b]{top:80;left:80;width:40;height:40;box-shadow:200px 0 #62306d,0 100px #62306d,200px 100px #62306d}a[c]{top:90;left:230;width:20;height:20;box-shadow:0 100px #62306d}</style>',
+  battle: BATTLES_MAP['battle003'],
+}
