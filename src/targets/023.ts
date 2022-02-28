37cab3ae7b9cab40df86d51fd642f0b1eae8c74a
felipeog <felipeog476@gmail.com>
2022-02-28 00:36:55 -0300

    Add 23

---
+++ src/targets/023.ts
@@ -0,0 +1,10 @@
+import { BATTLES_MAP } from '../consts/battlesMap'
+
+export default {
+  targetId: 'target023',
+  title: 'Target #23 - Boxception',
+  url: 'https://cssbattle.dev/play/23',
+  solution:
+    '<a a><a b><a c><style>body{background:#f3ac3c}a{position:fixed}a[a]{top:50;left:100;background:#1a4341;width:200;height:200}a[b]{top:150;left:100;background:#998235;width:100;height:100}a[c]{top:200;left:150;background:#f3ac3c;width:50;height:50}</style>',
+  battle: BATTLES_MAP['battle004'],
+}
