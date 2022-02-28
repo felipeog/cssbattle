8bd19e8b3d24ec5d41e8aedc07d2d3fa3e283e5b
felipeog <felipeog476@gmail.com>
2022-02-28 14:32:38 -0300

    Add 26, 27 and 28

---
+++ src/targets/026.ts
@@ -0,0 +1,10 @@
+import { BATTLES_MAP } from '../consts/battlesMap'
+
+export default {
+  targetId: 'target026',
+  title: 'Target #26 - Smiley',
+  url: 'https://cssbattle.dev/play/26',
+  solution:
+    '<a a><a b><a c><style>body{background:#6592cf}a{position:fixed;border:20px solid #060f55;width:80;height:80;border-radius:50%;clip:rect(auto,auto,60,auto)}a[a],a[b]{top:40}a[a]{left:40}a[b]{right:40}a[c]{bottom:40;left:140;transform:rotate(180deg)}</style>',
+  battle: BATTLES_MAP['battle004'],
+}
---
+++ src/targets/028.ts
@@ -0,0 +1,10 @@
+import { BATTLES_MAP } from '../consts/battlesMap'
+
+export default {
+  targetId: 'target028',
+  title: 'Target #28 - Cups & Balls',
+  url: 'https://cssbattle.dev/play/28',
+  solution:
+    '<a a><a b><a c><style>body{background:#1a4341}a{position:fixed;width:50;height:50}a[a]{background:#998235;border-radius:50%;box-shadow:210px 0 #f3ac3c,70px 70px #998235,140px 70px #f3ac3c}a[a],a[b]{left:70}a[b],a[c]{background:#f3ac3c}a[a],a[c]{top:90}a[b]{border-radius:0 0 25px 25px;box-shadow:210px 0 #998235;top:160}a[c]{border-radius:25px 25px 0 0;box-shadow:70px 0 #998235;left:140}</style>',
+  battle: BATTLES_MAP['battle004'],
+}
