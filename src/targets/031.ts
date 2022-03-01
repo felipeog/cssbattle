4c72d5d18e16db4a4e95fb077154cff3c472b7bf
felipeog <felipeog476@gmail.com>
2022-03-01 14:18:26 -0300

    Fix 31

--- src/targets/031.ts
+++ src/targets/031.ts
@@ -3,5 +3,5 @@ export default {
   title: 'Target #31 - Equals',
   url: 'https://cssbattle.dev/play/31',
   solution:
-    'body{background:#aa445f}a{position:fixed;top:50;width:100;height:200}a[a]{left:75;background:#f7ec7d;border-radius:100px 0 0 100px}a[b]{right:75;background:#e38f66;border-radius:0 100px 100px 0}',
+    '<a a><a b><style>body{background:#aa445f}a{position:fixed;top:50;width:100;height:200}a[a]{left:75;background:#f7ec7d;border-radius:100px 0 0 100px}a[b]{right:75;background:#e38f66;border-radius:0 100px 100px 0}</style>',
 }
