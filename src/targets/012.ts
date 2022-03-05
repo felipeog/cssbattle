9cf338c06c34db78cb8ac433acce5f34f6b0a7a1
felipeog <felipeog476@gmail.com>
2022-03-05 11:21:05 -0300

    Update 12

--- src/targets/012.ts
+++ src/targets/012.ts
@@ -3,5 +3,5 @@ export default {
   title: 'Target #12 - Wiggly Moustache',
   url: 'https://cssbattle.dev/play/12',
   solution:
-    '<a a><a b><a c><style>body{background:#F5D6B4}a{position:fixed;width:60;height:60;border-radius:50%;border:20px solid #D86F45;border-top-color:transparent;border-left-color:transparent;transform:rotate(45deg)}a[a]::after,a[c]::after{content:"";position:absolute;width:20;height:20;background:#D86F45;border-radius:50%}a[a]{top:100;left:70}a[a]::after{top:48;left:-8}a[b]{top:100;left:150;transform:rotate(225deg)}a[c]{top:100;left:230}a[c]::after{top:-8;left:48}</style>',
+    '<a a><a b><a c><a d><style>body{background:#f5d6b4}a{position:fixed;border-radius:50%}a[a],a[b],a[c]{top:100;width:60;height:60;border:20px solid #d86f45;clip:rect(50px,auto,auto,auto)}a[a]{left:70}a[b]{left:150;transform:rotate(180deg)}a[c]{right:70}a[d]{top:140;left:70;width:20;height:20;background:#d86f45;box-shadow:240px 0 #d86f45}</style>',
 }
