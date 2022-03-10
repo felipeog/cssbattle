36afa46428d51c1597386b35dae4a9d3372b7ab2
felipeog <felipeog476@gmail.com>
2022-03-10 08:51:32 -0300

    Remove unnecessary style

--- src/utils/getHtmlFromSolution.ts
+++ src/utils/getHtmlFromSolution.ts
@@ -1,6 +1,5 @@
 export function getHtmlFromSolution(solution: string) {
-  const head =
-    '<head><style>body{overflow:hidden;width:400px;height:300px}</style></head>'
+  const head = '<head><style>body{overflow:hidden}</style></head>'
   const body = `<body>${solution}</body>`
   const html = `<html>${head}${body}</html>`
 
