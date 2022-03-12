44d4f622be013f58b31650798deaa6eed01ad07a
felipeog <felipeog476@gmail.com>
2022-03-12 16:09:29 -0300

    Add pixels mismatch value to test output

--- src/tests/diff.spec.ts
+++ src/tests/diff.spec.ts
@@ -1,51 +1,27 @@
+import { PNG } from 'pngjs'
 import * as fs from 'fs'
 import * as path from 'path'
-import { PNG } from 'pngjs'
 import * as pixelmatch from 'pixelmatch'
-import htmlToImage from 'node-html-to-image'
 
-import * as targetsMap from '../targets'
+import { SOLVED_TARGETS } from '../consts/solvedTargets'
 import { TARGET_DIMENSIONS } from '../consts/targetDimensions'
-import { getHtmlFromSolution } from '../utils/getHtmlFromSolution'
 
-const allTargets = Object.values(targetsMap)
-const solvedTargets = allTargets.filter((target) => !!target.solution)
 const maximumPixelsMismatch =
   TARGET_DIMENSIONS.WIDTH * TARGET_DIMENSIONS.HEIGHT * 0.001
 
-async function createSolutionImage(solution: string) {
-  const solutionHtml = getHtmlFromSolution(solution)
-  const src = `data:text/html,${encodeURIComponent(solutionHtml)}`
-  const style = 'background:white;width:400px;height:300px;border:0;outline:0'
-
-  try {
-    const image = (await htmlToImage({
-      html: `<iframe src="${src}" style="${style}" />`,
-      encoding: 'base64',
-      puppeteerArgs: {
-        defaultViewport: {
-          width: TARGET_DIMENSIONS.WIDTH + 16,
-          height: TARGET_DIMENSIONS.HEIGHT,
-        },
-      },
-    })) as Buffer
-
-    fs.writeFileSync(path.resolve(__dirname, './solution.png'), image, 'base64')
-  } catch (error) {
-    throw Error(`createSolutionImage: ${error}`)
-  }
+function getPngFile(filePath: string) {
+  return PNG.sync.read(fs.readFileSync(filePath))
 }
 
-function getPixelsMismatch(fileName: string) {
-  const solution = PNG.sync.read(
-    fs.readFileSync(path.resolve(__dirname, './solution.png')),
-  )
-  const target = PNG.sync.read(
-    fs.readFileSync(path.resolve(__dirname, `./targetsImages/${fileName}`)),
-  )
+function getPixelsMismatch(
+  currentTargetFileName: string,
+  expectedTargetFileName: string,
+) {
+  const current = getPngFile(currentTargetFileName)
+  const expected = getPngFile(expectedTargetFileName)
   const pixelsMismatch = pixelmatch(
-    solution.data,
-    target.data,
+    current.data,
+    expected.data,
     null,
     TARGET_DIMENSIONS.WIDTH,
     TARGET_DIMENSIONS.HEIGHT,
@@ -54,16 +30,15 @@ function getPixelsMismatch(fileName: string) {
   return pixelsMismatch
 }
 
-describe(`pixels mismatch shoul be less than ${maximumPixelsMismatch}`, () => {
-  it.each(solvedTargets)('$id', async ({ id, solution }) => {
+describe.each(SOLVED_TARGETS)('$id pixels mismatch', ({ id }) => {
+  const fileName = `${Number(id)}.png`
+  const pixelsMismatch = getPixelsMismatch(
+    path.resolve(__dirname, `./targetsImages/${fileName}`),
+    path.resolve(__dirname, `./solutionsImages/${fileName}`),
+  )
+
+  it(`${pixelsMismatch} should be less than ${maximumPixelsMismatch}`, () => {
     expect.hasAssertions()
-
-    await createSolutionImage(solution)
-
-    const index = Number(id)
-    const fileName = `${index}.png`
-    const pixelsMismatch = getPixelsMismatch(fileName)
-
     expect(pixelsMismatch).toBeLessThan(maximumPixelsMismatch)
   })
 })
