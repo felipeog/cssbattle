a6b7c4f61690364d14695b80b016bfd901e19d25
felipeog <17603069+felipeog@users.noreply.github.com>
2022-03-19 13:34:54 -0300

    Add 67 and 68

--- src/targets/068.ts
+++ src/targets/068.ts
@@ -2,5 +2,6 @@ export default {
   id: '068',
   title: 'Target #68 - Bell',
   url: 'https://cssbattle.dev/play/68',
-  solution: '',
+  solution:
+    '<a a><a b><style>body{background:#191919}a{position:fixed}a[a]{top:66;left:175;width:50;height:50;border-radius:50%;background:#f2ad43;box-shadow:0 120px #824b20}a[b]{top:90;left:140;width:120;height:120;border-radius:60px 60px 10px 10px;background:#e08027}</style>',
 }
