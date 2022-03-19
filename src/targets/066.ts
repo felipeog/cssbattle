334fd1eb3e8278f41872907e04ed68d9e08975df
felipeog <17603069+felipeog@users.noreply.github.com>
2022-03-19 01:14:54 -0300

    Add 65 and 66

--- src/targets/066.ts
+++ src/targets/066.ts
@@ -2,5 +2,6 @@ export default {
   id: '066',
   title: 'Target #66 - Batmicky',
   url: 'https://cssbattle.dev/play/66',
-  solution: '',
+  solution:
+    '<a a><a b><a c><a d><a e><a f><style>body{background:#191919}a{position:fixed}a[a],a[e],a[f]{background:#F2AD43}a[b],a[c],a[d]{background:#191919}a[c],a[d],a[f]{border-radius:50%}a[a]{top:100;left:80;width:240;height:80}a[b]{top:70;left:160;width:80;height:60;border-radius:10px}a[c]{top:100;left:25;width:100;height:100;box-shadow:250px 0 #191919}a[d]{top:160;left:40;width:200;height:200;box-shadow:120px 0 #191919}a[e]{top:110;left:190;width:20;height:20}a[f]{top:105;left:185;width:10;height:10;box-shadow:20px 0 #F2AD43}</style>',
 }
