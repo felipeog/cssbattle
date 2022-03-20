4d45c3513487710071b4abeacca5b43eea6e98c1
felipeog <17603069+felipeog@users.noreply.github.com>
2022-03-20 16:29:19 -0300

    Add 69 and 70

--- src/targets/070.ts
+++ src/targets/070.ts
@@ -2,5 +2,6 @@ export default {
   id: '070',
   title: 'Target #70 - Froggy',
   url: 'https://cssbattle.dev/play/70',
-  solution: '',
+  solution:
+    '<a a><a b><style>body{background:#293462}a{position:fixed}a[a]{top:110;left:125;width:150;height:100;background:radial-gradient(ellipse 90% 77% at top,#FE5F55 90%,#A64942 91%);border-radius:60px}a[b]{background:#293462;top:105;left:160;width:10;height:10;border-radius:50%;box-shadow:0 0 0 10px #FFF1C1,0 0 0 20px #FE5F55,70px 0 #293462,70px 0 0 10px #FFF1C1,70px 0 0 20px #FE5F55,25px 55px #293462,45px 55px #293462}</style>',
 }
