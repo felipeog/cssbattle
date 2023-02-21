a9d8c839f6e70cab3266c6dd83cecd883f860249
felipeog <17603069+felipeog@users.noreply.github.com>
2023-02-21 13:16:53 -0300

    Cleanup

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,7 +1,6 @@
-import { HTMLAttributes, useEffect, useRef, useState } from 'react'
+import { HTMLAttributes } from 'react'
 
 import { IFrame } from '../IFrame'
-import { PlaceholderCard } from '../PlaceholderCard'
 import { Target } from '../../types'
 import { TARGET_DIMENSIONS } from '../../consts/targetDimensions'
 import * as styles from './index.module.css'
@@ -9,42 +8,15 @@ import * as styles from './index.module.css'
 export type CardProps = HTMLAttributes<HTMLDivElement> & Target
 
 export function Card({ solution, title }: CardProps) {
-  const cardRef = useRef()
-  const [isIntersecting, setIsIntersecting] = useState(true)
-
-  function handleObserverChange(entries: IntersectionObserverEntry[]) {
-    setIsIntersecting(entries?.[0]?.isIntersecting)
-  }
-
-  useEffect(() => {
-    if (!window?.IntersectionObserver) {
-      return
-    }
-
-    const observer = new IntersectionObserver(handleObserverChange, {
-      rootMargin: '100px',
-    })
-
-    observer.observe(cardRef.current)
-
-    return () => {
-      observer.unobserve(cardRef.current)
-    }
-  }, [])
-
   return (
-    <div className="Card" title={title} ref={cardRef}>
-      {isIntersecting ? (
-        <IFrame
-          className={styles.iframe}
-          width={TARGET_DIMENSIONS.WIDTH}
-          height={TARGET_DIMENSIONS.HEIGHT}
-          solution={solution}
-          title={title}
-        />
-      ) : (
-        <PlaceholderCard />
-      )}
+    <div className="Card" title={title}>
+      <IFrame
+        className={styles.iframe}
+        width={TARGET_DIMENSIONS.WIDTH}
+        height={TARGET_DIMENSIONS.HEIGHT}
+        solution={solution}
+        title={title}
+      />
     </div>
   )
 }
