4a6873610cebe4c49e2cedc0077ca8219fc6b0c4
felipeog <17603069+felipeog@users.noreply.github.com>
2023-09-17 17:57:51 -0300

    Add targets information scraper

---
+++ scripts/scrapeTargetsInformation.ts
@@ -0,0 +1,53 @@
+import * as fs from 'fs'
+import * as path from 'path'
+
+type Target = {
+  id: number
+  name: string
+  image: string
+}
+
+async function scrapeTargetsInformation(
+  index: number = 1,
+  targets: Target[] = [],
+) {
+  console.log(index)
+
+  const response = await fetch(`https://cssbattle.dev/play/${index}`)
+  const text = await response.text()
+  const matches = [
+    text.match(/"id":.+?,/g),
+    text.match(/"name":.+?,/g),
+    text.match(/"image":.+?,/g),
+  ]
+
+  if (matches.some((match) => !match)) {
+    fs.writeFileSync(
+      path.resolve(__dirname, `../shared/targets.json`),
+      JSON.stringify(targets),
+      'utf-8',
+    )
+
+    return console.log('end')
+  }
+
+  const targetJsonString = matches.reduce((previous, current, index, array) => {
+    const isLast = index + 1 === array.length
+    const next = current![0]
+
+    if (isLast) {
+      const nextWithoutComma = next.slice(0, -1)
+
+      return previous + nextWithoutComma + '}'
+    }
+
+    return previous + next
+  }, '{')
+
+  const target = JSON.parse(targetJsonString)
+  const newTargets = [...targets, target]
+
+  scrapeTargetsInformation(index + 1, newTargets)
+}
+
+scrapeTargetsInformation()
