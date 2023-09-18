c2946b099c957928c59d545400b9f3f74db3b9ff
Felipe OG <felipeog476@gmail.com>
2023-09-18 15:35:19 +0000

    Add 74

--- src/targets/074.ts
+++ src/targets/074.ts
@@ -2,5 +2,6 @@ export default {
   id: '074',
   title: 'Target #74 - Danger Noodle',
   url: 'https://cssbattle.dev/play/74',
-  solution: '',
+  solution:
+    '<p a><p b><p c><p d><p e><p f><p g><p h><style>body,p{background:#191919}[b],[d]{top:24px}[c],[e]{top:44px}p{width:70px;height:200px;position:absolute;box-shadow:inset 0 0 0 5px #e08027,inset 0 0 0 15px #191919,inset 0 0 0 20px #e08027;border-radius:35px}[a]{left:-35px;top:-56px;clip:rect(110px,auto,auto,auto)}[b]{left:15px;clip:rect(auto,auto,80px,auto)}[c]{left:65px;clip:rect(35px,auto,auto,auto)}[d]{left:115px;clip:rect(auto,auto,165px,auto)}[e]{left:165px;clip:rect(120px,auto,auto,auto)}[f]{width:99px;border-top-right-radius:0;left:215px;top:124px;clip:rect(auto,55px,80px,auto)}[g]{box-shadow:none;background:#e08027;width:50px;height:30px;top:119px;left:265px;border-radius:14px 20px 20px 14px}[h]{width:10px;height:10px;top:121px;left:295px;box-shadow:0 16px #191919}</style>',
 }
