6d29b3aaaeb859a21f3a30fb2d2fa0924fd489cb
felipeog <17603069+felipeog@users.noreply.github.com>
2023-09-24 15:07:33 -0300

    Add 131 and 135

--- shared/targets/135.ts
+++ shared/targets/135.ts
@@ -2,5 +2,6 @@ export default {
   id: '135',
   title: 'Target #135 - Spikes',
   url: 'https://cssbattle.dev/play/135',
-  solution: '',
+  solution:
+    '<a a><a b><style>[b],body{background:#e3516e}a{position:fixed}[a]{top:100;left:0;width:190;height:40;background:#d9d9d9;border-radius:9in;box-shadow:0 60px #d9d9d9,210px 0 #d9d9d9,210px 60px #d9d9d9}[b]{top:0;left:19;width:212;height:212;transform-origin:top left;transform:rotate(45deg);box-shadow:256px -256px #e3516e}</style>',
 }
