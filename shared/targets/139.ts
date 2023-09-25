57d9084a85659ad075c0d2cd101346aaa0f608e3
Felipe OG <felipeog476@gmail.com>
2023-09-25 13:59:08 +0000

    Add 129 and 139

--- shared/targets/139.ts
+++ shared/targets/139.ts
@@ -2,5 +2,6 @@ export default {
   id: '139',
   title: 'Target #139 - Lamp',
   url: 'https://cssbattle.dev/play/139',
-  solution: '',
+  solution:
+    '<a a><a b><style>body{background:#e38f66}a{position:fixed}[a]{top:90;left:137;width:127;height:120;background:#fffbda;clip-path:polygon(32px 0,94px 0,126px 120px,0 120px)}[b]{bottom:0;left:195;width:10;height:90;background:#62306d}</style>',
 }
