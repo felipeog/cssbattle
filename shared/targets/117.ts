2b62ad9cc56d40222c3b963b71be6f5eaaa55307
felipeog <17603069+felipeog@users.noreply.github.com>
2023-10-03 19:01:10 -0300

    Add 117 and 133

--- shared/targets/117.ts
+++ shared/targets/117.ts
@@ -2,5 +2,6 @@ export default {
   id: '117',
   title: 'Target #117 - Arineo',
   url: 'https://cssbattle.dev/play/117',
-  solution: '',
+  solution:
+    '<a a><a b><a c><a d><a e><style>[a],[b]{top:60;width:44;height:180;background:#2e2926}[c],[d]{height:28;background:#0088ca;border:11.5px solid #fff}body{background:radial-gradient(circle,#0088ca 11px,#fff 11px)}a{position:fixed}[a]{left:134;transform:skew(-25deg)}[b]{right:134;transform:skew(25deg)}[c]{top:138;left:120;width:80;transform:skewy(45deg);border-left:none;border-right:none}[d]{top:118;left:200;width:120;transform:skewy(-45deg);border-left:none;border-right:none}</style>',
 }
--- shared/targets/133.ts
+++ shared/targets/133.ts
@@ -2,5 +2,6 @@ export default {
   id: '133',
   title: 'Target #133 - Spiral',
   url: 'https://cssbattle.dev/play/133',
-  solution: '',
+  solution:
+    '<a a><a b><a c><a d><a e><style>[a],[c]{clip-path:inset(50% 0 0 0)}[b],[d]{clip-path:inset(0 0 50% 0)}[a],[e]{left:80}body{background:#f5d6b4}a{position:fixed}[a]{top:50;width:160;height:160;border:20px solid #d86f45;border-radius:9in}[b],[c]{left:120;border:20px solid #d86f45}[b]{top:70;width:120;height:120;border-radius:9in}[c]{top:90;width:80;height:80;border-radius:9in}[d]{top:110;left:160;width:40;height:40;border:20px solid #d86f45;border-radius:9in}[e]{top:140;width:20;height:20;border-radius:9in;background:#d86f45;box-shadow:80px 0 #d86f45}</style>',
 }
