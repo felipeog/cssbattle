0ad88812c6350ab05297a3891e221ee7ff70f235
Felipe OG <felipeog476@gmail.com>
2023-10-04 13:17:36 +0000

    Add 152, 153, and 154

--- shared/targets/153.ts
+++ shared/targets/153.ts
@@ -2,5 +2,6 @@ export default {
   id: '153',
   title: 'Target #153 - Checkers',
   url: 'https://cssbattle.dev/play/153',
-  solution: '',
+  solution:
+    '<a a><a b><style>body{background:#e3516e}a{position:fixed;background:#fade8b;color:#fade8b}[a]{top:50;left:100;width:40;height:40;box-shadow:80px 0,160px 0,40px 40px,120px 40px,0 80px,80px 80px,160px 80px,40px 120px,120px 120px,0 160px,80px 160px,160px 160px}[b]{top:60;left:150;width:20;height:20;border-radius:9in;box-shadow:80px 0,-40px 40px,40px 40px,120px 40px,-40px 120px,40px 120px,120px 120px,0 160px,80px 160px}</style>',
 }
--- shared/targets/154.ts
+++ shared/targets/154.ts
@@ -2,5 +2,6 @@ export default {
   id: '154',
   title: 'Target #154 - Poker Chip',
   url: 'https://cssbattle.dev/play/154',
-  solution: '',
+  solution:
+    '<a a><a b><a c><a d><a e><style>[a],[b],[c],[d]{left:185;width:30;background:#0b2429}[a],[b],[c],[d],[e]{top:0;height:300}body{background:#fcbe5c}a{position:fixed}[b]{transform:rotate(45deg)}[c]{transform:rotate(90deg)}[d]{transform:rotate(135deg)}[e]{left:0;width:400;background:radial-gradient(circle,#0b2429 60px,#fcbe5c 60px 70px,#0b2429 70px 80px,transparent 80px 95px,#0b2429 95px 105px,#998235 105px)}</style>',
 }
