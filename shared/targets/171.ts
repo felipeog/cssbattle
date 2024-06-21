9cc16430350800a799533726efe8def00608cd96
felipeog <17603069+felipeog@users.noreply.github.com>
2024-06-20 23:42:08 -0300

    Add 171 and 172

--- shared/targets/171.ts
+++ shared/targets/171.ts
@@ -2,5 +2,6 @@ export default {
   id: '171',
   title: 'Target #171 - Pumpkin',
   url: 'https://cssbattle.dev/play/171',
-  solution: '',
+  solution:
+    '<a a><a b><style>body{background:#784972}a{position:fixed;background:#f7ffcf}a[a]{width:100;height:150;border-radius:100%;top:100;left:150;box-shadow:0 0 0 10px #784972,-40px 0 #f7ffcf,-40px 0 0 10px #784972,-80px 0 #f7ffcf,-80px 0 0 10px #784972,40px 0 #f7ffcf,40px 0 0 10px #784972,80px 0 #f7ffcf,80px 0 0 10px #784972}a[b]{width:22;height:100;border-radius:9rem;top:50;left:189}</style>',
 }
