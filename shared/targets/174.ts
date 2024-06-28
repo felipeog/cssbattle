6112990f2d8e36199c24b0a181327ef516c3195d
felipeog <17603069+felipeog@users.noreply.github.com>
2024-06-27 22:08:56 -0300

    Add 173 and 174

--- shared/targets/174.ts
+++ shared/targets/174.ts
@@ -2,5 +2,6 @@ export default {
   id: '174',
   title: "Target #174 - Witch's Hat",
   url: 'https://cssbattle.dev/play/174',
-  solution: '',
+  solution:
+    '<a a><a b><a c><style>body{background:#f4dcbf}a{position:fixed}a[a]{top:50;left:155;width:90;height:153;clip-path:polygon(0 100%,50% 0,100% 100%);background:#30383f}a[b]{top:190;left:125;width:150;height:60;border-radius:100%;background:radial-gradient(ellipse 150px 60px at top,#30383f 43%,#556d7f 43% 57%,#30383f 57%)}a[c]{top:210;left:185;width:30;height:20;border-radius:5px;background:#fbd038}</style>',
 }
