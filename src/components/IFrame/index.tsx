4c89df96709861f4ddb1dca5993ebb5c9c3b5d07
Felipe OG <felipeog476@gmail.com>
2025-10-13 21:44:08 +0000

    fix iframe

--- src/components/IFrame/index.tsx
+++ src/components/IFrame/index.tsx
@@ -1,20 +1,12 @@
 import { IframeHTMLAttributes } from 'react'
 
-export function getHtmlFromSolution(solution: string) {
-  const head = '<head><style>body{overflow:hidden}</style></head>'
-  const body = `<body>${solution}</body>`
-  const html = `<html>${head}${body}</html>`
-
-  return html
-}
-
 export type IFrameProps = IframeHTMLAttributes<HTMLIFrameElement> & {
   solution: string
 }
 
 // https://stackoverflow.com/a/52873226
 export function IFrame({ solution, ...props }: IFrameProps) {
-  const html = getHtmlFromSolution(solution)
+  const html = `<style>body{overflow:hidden}</style>${solution}`
 
   return (
     <iframe src={'data:text/html,' + encodeURIComponent(html)} {...props} />
