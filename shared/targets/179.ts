0cbee0a16ed02fc558fd2cfc0e9d8c41042dadd2
Felipe OG <felipeog476@gmail.com>
2025-10-13 21:31:15 +0000

    add 179 and 180

--- shared/targets/179.ts
+++ shared/targets/179.ts
@@ -2,5 +2,6 @@ export default {
   id: '179',
   title: 'Target #179 - Tanzania',
   url: 'https://cssbattle.dev/play/179',
-  solution: '',
+  solution:
+    '<style>body{background:linear-gradient(-30deg,#3b66b0 160px,#f8cd1e 160px 180px,#000 180px 280px,#f8cd1e 280px 300px,#14a84a 300px)}</style>',
 }
