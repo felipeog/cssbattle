f3b939c6915dd0acf3df83ec7f5e8fa2c10bded6
Felipe OG <felipeog476@gmail.com>
2025-10-14 04:01:03 +0000

    add 185 and 186

--- shared/targets/185.ts
+++ shared/targets/185.ts
@@ -2,5 +2,6 @@ export default {
   id: '185',
   title: 'Target #185 - South Korea',
   url: 'https://cssbattle.dev/play/185',
-  solution: '',
+  solution:
+    '<p b><p c><p a><style>p{position:fixed}p[a]{width:120;height:120;top:74;left:140;border-radius:50%;background:radial-gradient(circle at 25% 50%,#cd2e3a 30px,transparent 30px),radial-gradient(circle at 75% 50%,#0047a0 30px,transparent 30px),linear-gradient(#cd2e3a 50%,#0047a0 50%)}p[b],p[c]{width:260;height:60;top:43}p[b]{left:102.5;transform-origin:top left;transform:rotate(30deg);background:linear-gradient(90deg,#000 10px,transparent 10px 16px,#000 16px 26px,transparent 26px 32px,#000 32px 42px,transparent 42px),linear-gradient(transparent 27px,#fff 27px 33px,transparent 33px),linear-gradient(-90deg,#000 10px,transparent 10px 16px,#000 16px 26px,transparent 26px 32px,#000 32px 42px,transparent 42px)}p[c]{right:102.5;transform-origin:top right;transform:rotate(-30deg);background:linear-gradient(90deg,#000 10px,transparent 10px 32px,#000 32px 42px,transparent 42px),linear-gradient(-90deg,transparent 16px,#000 16px 26px,transparent 26px),linear-gradient(transparent 27px,#fff 27px 33px,transparent 33px),linear-gradient(90deg,transparent 16px,#000 16px 26px,transparent 26px),linear-gradient(-90deg,#000 10px,transparent 10px 32px,#000 32px 42px,transparent 42px)}</style>',
 }
--- shared/targets/186.ts
+++ shared/targets/186.ts
@@ -2,5 +2,6 @@ export default {
   id: '186',
   title: 'Target #186 - Guernsey',
   url: 'https://cssbattle.dev/play/186',
-  solution: '',
+  solution:
+    '<p c><p d><p a><p b><style>body{background:linear-gradient(transparent 110px,#e8112d 110px 190px,transparent 190px),linear-gradient(90deg,transparent 160px,#e8112d 160px 240px,transparent 240px)}p{position:fixed}p[a],p[b],p[c],p[d]{width:200;height:20}p[a],p[b]{top:114;left:85;border-width:10 15;border-style:solid;border-color:transparent #f9dd16}p[c],p[d]{top:124;left:100;background:#f9dd16}p[b],p[d]{transform-origin:center;transform:rotate(90deg)}</style>',
 }
