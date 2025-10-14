6e4031c844eab4822e8530fb9b1b6bc3eb75a03a
Felipe OG <felipeog476@gmail.com>
2025-10-14 15:22:18 +0000

    add 243, 244, 245, and 246

--- shared/targets/243.ts
+++ shared/targets/243.ts
@@ -2,5 +2,6 @@ export default {
   id: '243',
   title: 'Target #243 - Corn',
   url: 'https://cssbattle.dev/play/243',
-  solution: '',
+  solution:
+    '<p a><p b><p c><p d><style>p[b],p[c],p[d]{background:#727272}body{background:#2f2f2f}p{position:fixed}p[a]{background:linear-gradient(90deg,transparent 20px,#2f2f2f 20px 30px,transparent 30px 50px,#2f2f2f 50px 60px,transparent 60px 80px,#2f2f2f 80px 90px,transparent 90px 110px,#2f2f2f 110px 120px,#c1c1c1 120px),linear-gradient(transparent 20px,#2f2f2f 20px 30px,transparent 30px 50px,#2f2f2f 50px 60px,transparent 60px 80px) #c1c1c1;top:94;left:50;width:260;height:80;border-radius:1in}p[b],p[c]{width:130;height:50}p[b]{top:89;left:180;border-radius:0 68px 0 1in}p[c]{top:129;left:180;border-radius:1in 0 68px}p[d]{top:124;left:310;width:40;height:20',
 }
--- shared/targets/246.ts
+++ shared/targets/246.ts
@@ -2,5 +2,6 @@ export default {
   id: '246',
   title: 'Target #246 - Limbs',
   url: 'https://cssbattle.dev/play/246',
-  solution: '',
+  solution:
+    '<p a><p b><p c><p d><style>p[a],p[b]{width:292;height:128;left:43}p[c],p[d]{width:64;height:99;left:107;border-style:solid;border-color:#333}body{background:#333}p{position:fixed;background:#eee}p[a]{top:19;border-radius:0 0 64px 64px}p[b]{top:127;border-radius:64px 64px 0 0}p[c]{top:-16;border-width:0 50px 44px}p[d]{top:147;border-width:44px 50px 0',
 }
