2b04bfc7abf0db919268432e1b5213f5577bc18c
Felipe OG <felipeog476@gmail.com>
2025-10-19 15:02:17 +0000

    generate empty targets

---
+++ shared/targets/247.ts
@@ -0,0 +1,6 @@
+export default {
+  id: '247',
+  title: 'Target #247 - LEDs',
+  url: 'https://cssbattle.dev/play/247',
+  solution: '',
+}
---
+++ shared/targets/248.ts
@@ -0,0 +1,6 @@
+export default {
+  id: '248',
+  title: 'Target #248 - Bolt',
+  url: 'https://cssbattle.dev/play/248',
+  solution: '',
+}
