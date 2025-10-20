589e74c6221ecc92892bf95364b52d398dfc4a48
Felipe OG <felipeog476@gmail.com>
2025-10-20 14:32:38 +0000

    add 205,  and 206

--- shared/targets/206.ts
+++ shared/targets/206.ts
@@ -2,5 +2,6 @@ export default {
   id: '206',
   title: 'Target #206 - Punisher',
   url: 'https://cssbattle.dev/play/206',
-  solution: '',
+  solution:
+    '<p a><p b><p c><p d><p e><p f><style>p[a],p[b],p[c]{background:#000}p[d],p[e]{top:119;width:46;height:23;background:#cae5dc;border-radius:0 0 1in 1in}body{background:#cae5dc}p{position:fixed;margin:0}p[a]{top:40;left:130;width:140;height:140;border-radius:1in}p[b]{top:150;left:150;width:100;height:30}p[c]{top:180;left:162;width:16;height:93;box-shadow:20px 0,40px 0,60px 0}p[d]{left:139;rotate:28deg}p[e]{right:139;rotate:-28deg}p[f]{top:136;left:189.5;border-style:solid;border-color:#cae5dc #000;border-width:0 10px 30px',
 }
