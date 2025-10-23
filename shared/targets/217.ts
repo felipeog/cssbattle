101e22f9b89f75e7c51e557ba6d6c003d18d5b2d
Felipe OG <felipeog476@gmail.com>
2025-10-23 00:59:46 +0000

    add 217, and 218

--- shared/targets/217.ts
+++ shared/targets/217.ts
@@ -2,5 +2,6 @@ export default {
   id: 217,
   title: 'Target #217 - Maze',
   url: 'https://cssbattle.dev/play/217',
-  solution: '',
+  solution:
+    '<p a><p b><style>p[a],p[b]{position:fixed;margin:0;width:50;height:50;transform:skewy(30deg)}body{background:#202939}p[a]{top:70;left:120;background:#556787;box-shadow:110px 48px #556787}p[b]{top:145;left:155;background:#fff;box-shadow:20px -31px #fff,40px -63px #fff',
 }
