aca213be6c560fafd1ac38a4fc8f4029c8a629dc
felipeog <17603069+felipeog@users.noreply.github.com>
2025-10-23 22:08:35 -0300

    update shared types

--- shared/types/index.ts
+++ shared/types/index.ts
@@ -1,6 +1,15 @@
+export type Battle = {
+  id: number
+  title: string
+  description: string
+  url: string
+  targets: number[]
+}
+
 export type Target = {
-  id: string
+  id: number
   title: string
   url: string
   solution: string
+  colors: string[]
 }
