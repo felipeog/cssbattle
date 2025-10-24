caafb93e4c8669dab22a41d9d95cd7cbd3c2ab32
felipeog <17603069+felipeog@users.noreply.github.com>
2025-10-23 22:08:21 -0300

    add target colors

--- scripts/generateTargetFiles.ts
+++ scripts/generateTargetFiles.ts
@@ -1,47 +1,44 @@
 import * as fs from 'fs'
 
 import { createFolder } from './utils/createFolder'
-import * as targets from 'shared/targetsInfo.json'
+import * as targetsInfo from 'shared/targetsInfo.json'
+import * as targetsMap from 'shared/targets'
 import { SHARED_FOLDER_PATH } from 'shared/consts/sharedFolderPath'
 
 export type TargetInfo = {
   id: number
   name: string
   image: string
+  colors: string[]
 }
 
 const targetsFolderPath = `${SHARED_FOLDER_PATH}/targets`
 
 function generateTargetFiles() {
-  targets.forEach((target) => {
+  targetsInfo.forEach((target) => {
     const targetFileName = `${target.id}.ts`
-    const targetFilePath = `${targetsFolderPath}/${targetFileName}`
-
-    if (fs.existsSync(targetFilePath)) {
-      return console.log(`${target.name} already exists, skipping...`)
-    }
-
-    console.log(`${target.name} does not exist`)
-    console.log(`Creating ${target.name}...`)
+    const solution = targetsMap?.[`target${target.id}`]?.solution || ''
 
     fs.writeFileSync(
       `${targetsFolderPath}/${targetFileName}`,
-      getFileContent(target),
+      getFileContent(target, solution),
     )
 
     console.log(`${target.name} created`)
   })
 }
 
-function getFileContent(target: TargetInfo) {
+function getFileContent(target: TargetInfo, solution = '') {
   const parsedName = target.name.replaceAll("'", "\\'")
+  const parsedColors = target.colors.map((color) => `'${color}'`).join(', ')
 
   return (
     `export default {\n` +
     `  id: ${target.id},\n` +
     `  title: 'Target #${target.id} - ${parsedName}',\n` +
     `  url: 'https://cssbattle.dev/play/${target.id}',\n` +
-    `  solution: '',\n` +
+    `  colors: [${parsedColors}],\n` +
+    `  solution: '${solution}',\n` +
     `}\n`
   )
 }
