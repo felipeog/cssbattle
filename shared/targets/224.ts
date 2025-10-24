89aa89c20ccdf4d5498a4b39810d1d5adb0e9e89
Felipe OG <felipeog476@gmail.com>
2025-10-24 02:56:46 +0000

    add 223, and 224

--- shared/targets/224.ts
+++ shared/targets/224.ts
@@ -3,5 +3,6 @@ export default {
   title: 'Target #224 - Levelled',
   url: 'https://cssbattle.dev/play/224',
   colors: ['#F8B140', '#000000', '#FFFFFF', '#67AED4'],
-  solution: '',
+  solution:
+    '<p a><p b><style>p[a],p[b]{left:125;border-radius:1in}body{background:#f8b140}p{position:fixed;margin:0}p[a]{bottom:75;width:150;height:150;background:linear-gradient(#fff 75px,#67aed4 75px);box-shadow:0 0 0 15px #fff,0 0 0 45px #000}p[b]{top:125;width:50;height:50;background:#67aed4;box-shadow:50px 0 #fff,100px 0 #67aed4}',
 }
