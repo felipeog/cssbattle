27f79d964741f59825fc1b462fbe76028fc20159
Felipe OG <felipeog476@gmail.com>
2025-10-25 18:58:31 +0000

    add 229, and 230

--- shared/targets/232.ts
+++ shared/targets/232.ts
@@ -3,5 +3,6 @@ export default {
   title: 'Target #232 - GAAD',
   url: 'https://cssbattle.dev/play/232',
   colors: ['#FDFFE9', '#01385E', '#81CAFE'],
-  solution: '',
+  solution:
+    '<p a><p b><p c><style>p[a],p[c]{background:#01385e}body{background:#fdffe9}p{position:fixed;margin:0}p[a]{top:90;left:180;width:40;height:70;box-shadow:-10px 20px #fdffe9,-30px 20px #01385e}p[b]{top:150;left:190;width:40;height:40;border:20px solid #01385e;border-radius:0 40px 0 0}p[c]{top:150;left:130;width:100;height:100;border-radius:1in;box-shadow:20px -130px 0 -30px #01385e,inset 0 0 0 20px #01385e,inset 0 0 0 40px #81cafe',
 }
